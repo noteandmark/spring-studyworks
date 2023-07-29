67de1c5771bd38cb1f34732a85bfd7ac99e4c0c5
Andrei Markov <noteandmark@gmail.com>
2023-07-29 19:55:57 +0300

    optimize query for index persons

--- bookkeeping/src/main/java/com/home/andmark/bookkeeping/dao/PersonDAO.java
+++ bookkeeping/src/main/java/com/home/andmark/bookkeeping/dao/PersonDAO.java
@@ -1,6 +1,31 @@
 package com.home.andmark.bookkeeping.dao;
 
 import com.home.andmark.bookkeeping.model.Person;
+import org.hibernate.Session;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface PersonDAO extends AbstractDAO<Person> {
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+
+    private final EntityManager entityManager;
+
+    public PersonDAO(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Person> readAllPersons() {
+        Session session = entityManager.unwrap(Session.class);
+
+        List<Person> persons = session.createQuery("select distinct p from Person p " +
+                        "LEFT JOIN FETCH p.books")
+                .getResultList();
+
+        System.out.println(persons);
+        return persons;
+    }
 }
