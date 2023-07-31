068363e19d48eca4934026becc8ce7ce5adb39bb
Andrei Markov <noteandmark@gmail.com>
2023-07-31 20:27:12 +0300

    add mapping for showUserInfo

--- bookkeepingsb/src/main/java/com/home/andmark/bookkeepingsb/controller/HomePageController.java
+++ bookkeepingsb/src/main/java/com/home/andmark/bookkeepingsb/controller/HomePageController.java
@@ -1,6 +1,11 @@
 package com.home.andmark.bookkeepingsb.controller;
 
+import com.home.andmark.bookkeepingsb.security.PersonDetails;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -9,6 +14,14 @@ public class HomePageController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String welcome() {
+        return "index";
+    }
+
+    @GetMapping("/showUserInfo")
+    public String showUserInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        System.out.println(personDetails.getPerson());
         return "index";
     }
 
