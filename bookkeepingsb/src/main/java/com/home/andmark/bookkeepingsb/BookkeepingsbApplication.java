e6899166deef8cc85800ab2dfee7f86e3cea25ef
Andrei Markov <noteandmark@gmail.com>
2023-07-31 13:26:57 +0300

    add for remodelling bookkeeping in springboot app

---
+++ bookkeepingsb/src/main/java/com/home/andmark/bookkeepingsb/BookkeepingsbApplication.java
@@ -0,0 +1,13 @@
+package com.home.andmark.bookkeepingsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookkeepingsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookkeepingsbApplication.class, args);
+	}
+
+}
---
+++ bookkeepingsb/src/test/java/com/home/andmark/bookkeepingsb/BookkeepingsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.andmark.bookkeepingsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookkeepingsbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
