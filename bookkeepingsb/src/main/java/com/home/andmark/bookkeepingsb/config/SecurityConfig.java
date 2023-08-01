80b8f30feceda4f5faf190cc1765e9da852df350
Andrei Markov <noteandmark@gmail.com>
2023-08-01 12:15:51 +0300

    authentication using the UserDetailsService interface

--- bookkeepingsb/src/main/java/com/home/andmark/bookkeepingsb/config/SecurityConfig.java
+++ bookkeepingsb/src/main/java/com/home/andmark/bookkeepingsb/config/SecurityConfig.java
@@ -1,23 +1,31 @@
 package com.home.andmark.bookkeepingsb.config;
 
-import com.home.andmark.bookkeepingsb.security.AuthProviderImpl;
+import com.home.andmark.bookkeepingsb.service.impl.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
-    //configures authentication
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider);
+    //configures authenticat ion
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
